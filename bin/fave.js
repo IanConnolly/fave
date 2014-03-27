0242e4adc1615c5981a379b8e7ebae867da2875c
Ian Connolly <ian@connolly.io>
2014-03-27 20:32:04 +0000

    style changes

--- bin/fave.js
+++ bin/fave.js
@@ -1,17 +1,14 @@
 var config = require('../config.json');
 var Twit = require('twit');
-var argv = require('yargs')
-     .demand(1)
-     .argv;
+var argv = require('yargs').demand(1).argv;
 
 var search_term = argv._[0];
-
 var T = new Twit(config);
 var stream = T.stream('statuses/filter', { track: search_term });
 
 stream.on('tweet', function (tweet) {
   T.post('favorites/create', {id: tweet.id_str}, function(err, reply) {
-    if (err) console.log(err);
-    if (reply) console.log("Favorited tweet id: " + tweet.id_str);
+    if (err) console.error(err);
+    if (reply) console.log('Favorited tweet id: ' + tweet.id_str);
   });
 });
