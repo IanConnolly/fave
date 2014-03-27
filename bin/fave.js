d0d60ba48f2db2e4517d272c049d58a9ae7a4205
Ian Connolly <ian@connolly.io>
2014-03-27 13:51:21 +0000

    oh my god javascript and numbers what

--- bin/fave.js
+++ bin/fave.js
@@ -7,13 +7,11 @@ var argv = require('yargs')
 var search_term = argv._[0];
 
 var T = new Twit(config);
-
-
 var stream = T.stream('statuses/filter', { track: search_term });
 
 stream.on('tweet', function (tweet) {
-  T.post('favorites/create', {id: tweet.id}, function(err, reply) {
-    if (err) console.err(err);
-    if (reply) console.log("Favorited tweet id: " + tweet.id);
+  T.post('favorites/create', {id: tweet.id_str}, function(err, reply) {
+    if (err) console.log(err);
+    if (reply) console.log("Favorited tweet id: " + tweet.id_str);
   });
 });
